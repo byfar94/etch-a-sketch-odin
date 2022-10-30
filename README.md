a948a619aca41644ed570665e384f8ed9b59934d
doug <byfar357@gmail.com>
2022-10-29 21:28:32 -0400

    improved UI, retro theme

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # etch-a-sketch-odin
 
+Etch-a-sketch project from The Odin Project, an open source webdevelopment program.
+
+topic learned
 CSS:
     using display: grid
+    adding font family from google fonts
 
 Javascript:
     for loop
     DOM manipulation and events
-
-
         adding CSS styling using Javascript
         adding classes using Javascript
--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 //html elements
 const etchContainer = document.querySelector(".etch");
 
-const increase = document.querySelector(".increase-grid");
+const slide = document.querySelector(".slider");
 
 const clearButton = document.querySelector(".clearbtn")
 
 const redButton = document.querySelector(".redbtn");
 
-const blackButton = document.querySelector(".blackbtn");
+const indigoButton = document.querySelector(".indigobtn");
 
-const blueButton = document.querySelector(".bluebtn");
+const cyanButton = document.querySelector(".cyanbtn");
 
-const yellowButton = document.querySelector(".yellowbtn");
+const pinkButton = document.querySelector(".pinkbtn");
+
+const eraserButton = document.querySelector(".eraserbtn");
+
 
 //will change class so that different css background colors take effect
 colorSelect = changeColor();
@@ -42,28 +45,24 @@ function changeColor (color){
 
 //function to remove all previous classes so that only one class will be present on box divs. Class names will need to be added to the array for this function to work correctly.
 function remColor (){
-    return ["red","black","blue","yellow"];
+    return ["red","indigo","cyan","pink","white"];
 }
 
-//execute createGrid(16) so that a basic grid with 16x16 divs is set up.
+//execute createGrid(16) so that a basic grid with 16x16 divs is set up initially
 createGrid(16);
 
+
 //function used to set size
-function setSize(){
-    //clear grid neeed to that new divs are not just added to the 
+function setSize(num){
     clearGrid();
-    let num = prompt("select the number of column you would like");
-    if (num > 1 && num <= 100){
     createGrid(num);
-    }else { 
-        createGrid(16);
-        alert("you must pick a number between 2 - 100");
-    }
 }
 
-//set number of rows/ columns for gird when you press increase button using function setsize
-increase.addEventListener("click", setSize);
-
+//slider input
+slide.oninput = function() {
+    let size = this.value;
+    setSize(size);
+  }
 
 //clears grid (removes div inside etchContainer)
 function clearGrid(){
@@ -71,48 +70,58 @@ function clearGrid(){
 }
 
 
-//creates blank 16*16 grid
+//creates blank equal to slider size
 function blankGrid (){
-    createGrid(16);
+    createGrid(slide.oninput());
 }
 
 //color changes **
 
-blackButton.addEventListener("click", function (){
-    changeColor("black");
-    blackButton.style.cssText = "background-color: black";
+indigoButton.addEventListener("click", function (){
+    changeColor("indigo");
+    /*blackButton.style.cssText = "background-color: black";
     redButton.style.cssText = "background-color: white";
     blueButton.style.cssText = "background-color: white";
-    yellowButton.style.cssText = "background-color: white";
+    yellowButton.style.cssText = "background-color: white";*/
 });
 
 redButton.addEventListener("click", function (){
     changeColor("red");
-    blackButton.style.cssText = "background-color: white";
+    /*blackButton.style.cssText = "background-color: white";
     redButton.style.cssText = "background-color: red";
     blueButton.style.cssText = "background-color: white";
-    yellowButton.style.cssText = "background-color: white";
+    yellowButton.style.cssText = "background-color: white";*/
 });
 
 
-blueButton.addEventListener("click", function (){
-    changeColor("blue");
-    blackButton.style.cssText = "background-color: white";
+cyanButton.addEventListener("click", function (){
+    changeColor("cyan");
+   /* blackButton.style.cssText = "background-color: white";
     redButton.style.cssText = "background-color: white";
     blueButton.style.cssText = "background-color: blue";
-    yellowButton.style.cssText = "background-color: white";
+    yellowButton.style.cssText = "background-color: white";*/
 });
 
-yellowButton.addEventListener("click", function (){
-    changeColor("yellow");
-    blackButton.style.cssText = "background-color: white";
+pinkButton.addEventListener("click", function (){
+    changeColor("pink");
+   /* blackButton.style.cssText = "background-color: white";
     redButton.style.cssText = "background-color: white";
     blueButton.style.cssText = "background-color: white";
-    yellowButton.style.cssText = "background-color: yellow";
+    yellowButton.style.cssText = "background-color: yellow";*/
 });
+
+eraserButton.addEventListener("click", function (){
+    changeColor("white")
+   /* blackButton.style.cssText = "background-color: white";
+    redButton.style.cssText = "background-color: white";
+    blueButton.style.cssText = "background-color: white";
+    yellowButton.style.cssText = "background-color: white";
+    eraserButton.style.cssText = "background-color: white"*/
+})
 
 //other Buttons
 
+//reset button event
 clearButton.addEventListener("click", function (){
     clearGrid();
     blankGrid();
